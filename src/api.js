17e464e5f2a9b072f7b66bece12f822a01279169
Aman Gupta <aman.gupta@livspace.com>
2021-05-09 12:14:19 +0530

    removed logs

--- src/api.js
+++ src/api.js
@@ -9,11 +9,7 @@ module.exports = async function (projectName, environment, build, fixtureName, s
         //console.log('*******************************' + projectName, environment, build, fixtureName, stepName, status, durationMs, screenShotArr, errorArr);
         // axios.post(url, { 'projectName': projectName, 'environment': environment, 'build': build, 'fixtureName': fixtureName, 'stepName': stepName, 'status': status, 'durationMs': durationMs, 'screenShotArr': screenShotArr, 'errorArr': errorArr, 'tags': tags })
         // .catch(err => console.log('Not able to post test through api', err));
-        axios.post('http://localhost:5000/posts/add', { 'projectName': projectName, 'environment': environment, 'build': build, 'fixtureName': fixtureName, 'stepName': stepName, 'status': status, 'durationMs': durationMs, 'screenShotArr': screenShotArr, 'errorArr': errorArr })
-            .then(function () {
-                console.log("request sent succesfully");
-            })
-            .catch(err => console.log('Not able to post test through api', err.message));
+        axios.post('http://localhost:5000/posts/add', { 'projectName': projectName, 'environment': environment, 'build': build, 'fixtureName': fixtureName, 'stepName': stepName, 'status': status, 'durationMs': durationMs, 'screenShotArr': screenShotArr, 'errorArr': errorArr });
     }
     catch (error) {
         console.log(error.message);
