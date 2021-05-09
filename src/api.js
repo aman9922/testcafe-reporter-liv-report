83479a9f1eaff954b7567d4e005645d0a5540c6a
Aman Gupta <aman.gupta@livspace.com>
2021-05-09 12:19:30 +0530

    removed logs

--- src/api.js
+++ src/api.js
@@ -6,13 +6,14 @@ const url = process.env.DASHBOARD_URL + '/add';
 module.exports = async function (projectName, environment, build, fixtureName, stepName, status, durationMs, screenShotArr, errorArr) {
     try {
         console.log(url + '*******************************');
-        //console.log('*******************************' + projectName, environment, build, fixtureName, stepName, status, durationMs, screenShotArr, errorArr);
+        console.log('*******************************' + projectName, environment, build, fixtureName, stepName, status, durationMs, screenShotArr, errorArr);
         // axios.post(url, { 'projectName': projectName, 'environment': environment, 'build': build, 'fixtureName': fixtureName, 'stepName': stepName, 'status': status, 'durationMs': durationMs, 'screenShotArr': screenShotArr, 'errorArr': errorArr, 'tags': tags })
         // .catch(err => console.log('Not able to post test through api', err));
         axios.post('http://localhost:5000/posts/add', { 'projectName': projectName, 'environment': environment, 'build': build, 'fixtureName': fixtureName, 'stepName': stepName, 'status': status, 'durationMs': durationMs, 'screenShotArr': screenShotArr, 'errorArr': errorArr });
     }
     catch (error) {
-        console.log(error.message);
+        //console.log(error.message);
+        console.log("Error while calling API");
     }
 
 
