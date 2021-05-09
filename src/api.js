3b432c2c1a894cff496b1013250cc90a845accc4
Aman Gupta <aman.gupta@livspace.com>
2021-05-09 16:41:10 +0530

    test

--- src/api.js
+++ src/api.js
@@ -9,7 +9,14 @@ module.exports = async function (projectName, environment, build, fixtureName, s
         //console.log('*******************************' + projectName, environment, build, fixtureName, stepName, status, durationMs, screenShotArr, errorArr);
         // axios.post(url, { 'projectName': projectName, 'environment': environment, 'build': build, 'fixtureName': fixtureName, 'stepName': stepName, 'status': status, 'durationMs': durationMs, 'screenShotArr': screenShotArr, 'errorArr': errorArr, 'tags': tags })
         // .catch(err => console.log('Not able to post test through api', err));
-        axios.post('http://localhost:5000/posts/add', { 'projectName': projectName, 'environment': environment, 'build': build, 'fixtureName': fixtureName, 'stepName': stepName, 'status': status, 'durationMs': durationMs, 'screenShotArr': screenShotArr, 'errorArr': errorArr });
+       // axios.post('http://localhost:5000/posts/add', { 'projectName': projectName, 'environment': environment, 'build': build, 'fixtureName': fixtureName, 'stepName': stepName, 'status': status, 'durationMs': durationMs, 'screenShotArr': screenShotArr, 'errorArr': errorArr });
+        axios({
+            method: 'post',
+            url: 'http://localhost:5000/posts/add',
+            data: { 'projectName': projectName, 'environment': environment, 'build': build, 'fixtureName': fixtureName, 'stepName': stepName, 'status': status, 'durationMs': durationMs, 'screenShotArr': screenShotArr, 'errorArr': errorArr },
+            maxContentLength: Infinity,
+            maxBodyLength: Infinity
+        });
     }
     catch (error) {
         //console.log(error.message);
