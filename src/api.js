4ae1ccd807a9361c6718b5ba2e44b7b45b94bc26
Aman Gupta <aman.gupta@livspace.com>
2022-01-17 11:20:21 +0530

    removed try catch

--- src/api.js
+++ src/api.js
@@ -4,41 +4,41 @@ const axios = require('axios');
 const url = process.env.DASHBOARD_URL + '/posts/add';
 
 module.exports = async function (projectName, environment, build, fixtureName, stepName, status, durationMs, screenShotArr, errorArr) {
-    try {
-        //console.log(url + '*******************************');
-        //console.log('*******************************' + projectName, environment, build, fixtureName, stepName, status, durationMs, screenShotArr, errorArr);
-        //console.log( process.env.CONTENTTYPE, process.env.AUTHORIZATION);
-        // axios.post(url, { 'projectName': projectName, 'environment': environment, 'build': build, 'fixtureName': fixtureName, 'stepName': stepName, 'status': status, 'durationMs': durationMs, 'screenShotArr': screenShotArr, 'errorArr': errorArr, 'tags': tags })
-        // .catch(err => console.log('Not able to post test through api', err));
-        
-        // axios.post(url, { 'projectName': projectName, 'environment': environment, 'build': build, 'fixtureName': fixtureName, 'stepName': stepName, 'status': status, 'durationMs': durationMs, 'screenShotArr': screenShotArr, 'errorArr': errorArr }, { headers:{ 
-        //     'Content-Type': process.env.CONTENTTYPE, 
-        //     'Authorization': process.env.AUTHORIZATION 
-        // } }).catch(function (error) {
-        //     // handle error
-        //     console.log(error);
-        // });
+    //try {
+    //console.log(url + '*******************************');
+    //console.log('*******************************' + projectName, environment, build, fixtureName, stepName, status, durationMs, screenShotArr, errorArr);
+    //console.log( process.env.CONTENTTYPE, process.env.AUTHORIZATION);
+    // axios.post(url, { 'projectName': projectName, 'environment': environment, 'build': build, 'fixtureName': fixtureName, 'stepName': stepName, 'status': status, 'durationMs': durationMs, 'screenShotArr': screenShotArr, 'errorArr': errorArr, 'tags': tags })
+    // .catch(err => console.log('Not able to post test through api', err));
 
-        axios({
-            method: 'post',
-            url: url,
-            data: { 'projectName': projectName, 'environment': environment, 'build': build, 'fixtureName': fixtureName, 'stepName': stepName, 'status': status, 'durationMs': durationMs, 'screenShotArr': screenShotArr, 'errorArr': errorArr },
-            maxContentLength: Infinity,
-            maxBodyLength: Infinity,
-            headers:{ 
-                'Content-Type': process.env.CONTENTTYPE, 
-                'Authorization': process.env.AUTHORIZATION 
-            }
+    // axios.post(url, { 'projectName': projectName, 'environment': environment, 'build': build, 'fixtureName': fixtureName, 'stepName': stepName, 'status': status, 'durationMs': durationMs, 'screenShotArr': screenShotArr, 'errorArr': errorArr }, { headers:{ 
+    //     'Content-Type': process.env.CONTENTTYPE, 
+    //     'Authorization': process.env.AUTHORIZATION 
+    // } }).catch(function (error) {
+    //     // handle error
+    //     console.log(error);
+    // });
+
+    axios({
+        method: 'post',
+        url: url,
+        data: { 'projectName': projectName, 'environment': environment, 'build': build, 'fixtureName': fixtureName, 'stepName': stepName, 'status': status, 'durationMs': durationMs, 'screenShotArr': screenShotArr, 'errorArr': errorArr },
+        maxContentLength: Infinity,
+        maxBodyLength: Infinity,
+        headers: {
+            'Content-Type': process.env.CONTENTTYPE,
+            'Authorization': process.env.AUTHORIZATION
+        }
+    })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
         });
-        // .catch(function (error) {
-        //     // handle error
-        //     console.log(error);
-        // });
-    }
-    catch (error) {
-        console.log('Error while calling API');
-        console.log(error.message);
-    }
+    // }
+    // catch (error) {
+    //     console.log('Error while calling API');
+    //     //console.log(error.message);
+    // }
 
 
 };
